895009a04dbb8889eb0385c49afadb1ddd8b3d5c
redei-ma <renato.deimarini@gmail.com>
2025-05-05 14:42:05 +0200

    norm

--- src/executor/ft_exec_1.c
+++ src/executor/ft_exec_1.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: renato <renato@student.42.fr>              +#+  +:+       +#+        */
+/*   By: redei-ma <redei-ma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 16:35:37 by redei-ma          #+#    #+#             */
-/*   Updated: 2025/03/22 12:35:42 by renato           ###   ########.fr       */
+/*   Updated: 2025/03/22 12:35:42 by redei-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,22 +61,56 @@ char	**ft_cmd_join(char *cmd, char **args, t_shell *shell)
 	return (command);
 }
 
-void	set_dups(t_cmd *cmd, t_shell *shell)
+void	set_dups(t_cmd *cmd, char *full_path, char **command, t_shell *shell)
 {
+	int	err;
+
+	err = 0;
 	if (cmd->file_i != -1)
 	{
 		if (dup2(cmd->file_i, STDIN_FILENO) == -1)
-			exit_all("dup2 input", shell, 1);
+			err = ft_printfd(2, "Error: dup2 input\n");
 	}
 	if (cmd->file_a != -1)
 	{
 		if (dup2(cmd->file_a, STDOUT_FILENO) == -1)
-			exit_all("dup2 append", shell, 1);
+			err = ft_printfd(2, "Error: dup2 append\n");
 	}
 	else if (cmd->file_o != -1)
 	{
 		if (dup2(cmd->file_o, STDOUT_FILENO) == -1)
-			exit_all("dup2 output", shell, 1);
+			err = ft_printfd(2, "Error: dup2 output\n");
+	}
+	if (err != 0)
+	{
+		free(full_path);
+		ft_free_char_mat(command);
+		exit_all(NULL, shell, 1);
+	}
+}
+
+void	get_exec_info(t_shell *shell, char **full_path,
+	char ***command, char ***copy_env)
+{
+	*full_path = get_path(shell->cmds->cmd, shell->env);
+	if (!*full_path)
+	{
+		*full_path = ft_strdup(shell->cmds->cmd);
+		if (!*full_path)
+			exit_all("Error: malloc failed\n", shell, 1);
+	}
+	*command = ft_cmd_join(shell->cmds->cmd, shell->cmds->args, shell);
+	if (!*command)
+	{
+		free(*full_path);
+		exit_all("Error: malloc failed\n", shell, 1);
+	}
+	*copy_env = copy_mat(shell->env, NULL, shell);
+	if (!*copy_env)
+	{
+		free(*full_path);
+		ft_free_char_mat(*command);
+		exit_all("Error: malloc failed\n", shell, 1);
 	}
 }
 
@@ -86,23 +120,8 @@ void	ft_exec(t_shell *shell)
 	char		**command;
 	char		**copy_env;
 
-	full_path = get_path(shell->cmds->cmd, shell->env);
-	if (!full_path)
-	{
-		full_path = ft_strdup(shell->cmds->cmd);
-		if (!full_path)
-			exit_all("Error: malloc failed\n", shell, 1); //non sarebbe proprio cosi
-	}
-	command = ft_cmd_join(shell->cmds->cmd, shell->cmds->args, shell);
-	if (!command)
-	{
-		free(full_path);
-		exit_all("Error: malloc failed\n", shell, 1);
-	}
-	set_dups(shell->cmds, shell);
-	// se fallisce non frea cio che ho rcreato qui
-	copy_env = copy_mat(shell->env, NULL, shell);
-	//se non  torna mi perdo cose da freaer qua
+	get_exec_info(shell, &full_path, &command, &copy_env);
+	set_dups(shell->cmds, full_path, command, shell);
 	close_all(shell);
 	free_all(shell);
 	execve(full_path, command, copy_env);
